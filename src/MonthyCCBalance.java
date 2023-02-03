b8acc0c1ec9fa783595ce3ed5ad8c109d614a7ac
Shamak10 <124183731+Shamak10@users.noreply.github.com>
2023-02-03 17:40:57 -0500

    Update MonthyCCBalance.java

--- src/MonthyCCBalance.java
+++ src/MonthyCCBalance.java
@@ -1,5 +1,8 @@
 public class MonthyCCBalance {
     public static void main(String[] args) {
-        System.out.println("");
+        double startingBalance= 5000;
+        double interestRate= 0.17;
+        System.out.println("Interest rate due after first month " + (startingBalance * interestRate));
+        System.out.println("Interest rate due after two month " + ((startingBalance + startingBalance * interestRate) * interestRate));
     }
 }
